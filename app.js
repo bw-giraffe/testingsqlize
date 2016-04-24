455cfb53b686a1aa8eb41a0af7d8d610dfa4cdc6
theletterrsquared@gmail.com <theletterrsquared@gmail.com>
2016-04-23 17:30:28 -0700

    trying different connection sync properties out

--- app.js
+++ app.js
@@ -4,13 +4,39 @@ var connection = new Sequelize('demodb', 'cestmoi', 'smartypass', {
 	dialect: 'postgres'
 });
 
+
+// var Book = connection.define('book', {
+// 	title: Sequelize.STRING
+// });
+
+// var Author = connection.define('author', {
+// 	name: Sequelize.STRING
+// });
+
+// Book.belongsTo(Author);
+
 var Article = connection.define('article', {
 	title: Sequelize.STRING,
 	body: Sequelize.TEXT
 });
 
-connection.sync();
+connection.sync().then(function() {
+	Article.findAll().then(function(article){
+		console.log("this is your article length", article.length);
+	});
+});
 
+// connection
+// 	.sync({
+// 		force: true,
+// 		logging: console.log
+// 	})
+// 	.then(function() {
+
+// 	})
+// 	.catch(function (error) {
+// 		console.log(error);
+// });
 // connection.sync().then(function() {
 // 	Article.create({
 // 		title: 'newentry',
@@ -18,8 +44,8 @@ connection.sync();
 // 	});
 // });
 
-connection.sync().then(function() {
-	Article.findById(1).then(function(article){
-		console.log(article.dataValues);
-	});
-});
+// connection.sync().then(function() {
+// 	Article.findById(1).then(function(article){
+// 		console.log(article.dataValues);
+// 	});
+// });
